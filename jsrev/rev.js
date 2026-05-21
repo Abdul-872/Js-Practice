a8bb6cf4af08ff215979e432900c9c728b6874a2
Abdul Ahad <abdulahadkhan8720@gmail.com>
2026-05-21 17:05:16 +0530

    This is Syllabus

---
+++ jsrev/rev.js
@@ -0,0 +1,38 @@
+console.log("Hello World");
+
+// let var const 
+//concatination 
+//operators
+//functions
+//objects
+//arrays
+//array methods
+//string methods
+//loops
+//conditionals
+//Async JS
+//promises
+//async await
+//fetch API
+//session storage
+//local storage
+//cookies
+//sessrion storage vs local storage
+//hoisting
+//scope
+//closure
+//this keyword
+//call apply bind
+//prototypes
+//debouncing
+//throttling
+//bubbling and capturing
+//event delegation
+//event propagation
+//event loop
+//call stack
+//web APIs
+//callback queue
+//microtask queue
+//event loop
+//Modules
